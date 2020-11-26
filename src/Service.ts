a3c4eb81cd38260fd011df8dc37f35482ae1710e
Ali Faris <alifaris@earthlink.iq>
2020-11-26 19:08:28 +0300

    service base

--- src/Service.ts
+++ src/Service.ts
@@ -5,3 +5,18 @@ export interface Service<T extends Autofiyable = Autofiyable> {
     getAutofiyable() : T;
 
 }
+
+
+export class ServiceBase<T extends Autofiyable = Autofiyable> implements Service<T> {
+
+    private readonly autofiyable : T;
+
+    constructor(autofiyable : T) {
+        this.autofiyable = autofiyable;
+    }
+
+    getAutofiyable() : T {
+        return this.autofiyable;
+    }
+
+}
