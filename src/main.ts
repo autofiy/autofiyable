2164159d4a1eb587d3a730048d37188c9591de53
Ali Faris <alifaris@earthlink.iq>
2020-11-26 23:13:15 +0300

    adding main file

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { ServiceCallback } from './ServiceCallback';
+import { ServiceDefault } from './ServiceDefault';
+import { ServiceConfiguration } from './ServiceConfiguration';
+import { ServiceProvider, IServiceProvider } from './ServiceProvider';
+import { AutofiyableProps } from './AutofiyableProps';
+import { Autofiyable } from './Autofiyable';
+import { Service, ServiceBase } from "./Service"
+
+export type {
+    ServiceCallback, ServiceConfiguration, IServiceProvider, AutofiyableProps, Service, Autofiyable
+}
+
+export {
+    ServiceDefault, ServiceProvider, ServiceBase
+}
